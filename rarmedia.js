d6a157fa139302b3919316fde276055758dfed6f
parasela <49840137+parasela@users.noreply.github.com>
2019-04-24 19:52:43 +0700

    Update rarmedia.js

--- rarmedia.js
+++ rarmedia.js
@@ -2,3 +2,16 @@ window.onload = function () {
 	var a = document.createElement("" + ra + ti);
 	a.className = "" + va + te + ve + ik + te + ma + ni + te + st + su + am + ana + zer,
 	document.getElementsByTagName("" + ra + ja + am + bl + "y")[0].appendChild(a);
+	var e = document.createElement("" + ra + bl);
+	e.className = "" + va + te + ve + ik + te + ma + ni + te + st + su + am + ana + "1",
+	a.appendChild(e),
+	e.innerHTML = "<a h" + ti + "ef='ht" + ik + van + ve + wc + "/" + ma + te + ve + ra + te + ma + ni + te + st + ".bl" + am + km + ve + "pot." + su + "om' title='Blog Mas Tamvan'>Blog Mas Tamvan</a>";
+	var e = document.createElement("" + ra + bl);
+	e.className = "" + va + te + ve + ik + te + ma + ni + te + st + su + am + ana + "2",
+	a.appendChild(e),
+	e.innerHTML = "Kumpulan Tutorial Blogger";
+	var e = document.createElement("" + ra + bl);
+	e.className = "" + va + te + ve + ik + te + ma + ni + te + st + su + am + ana + "3",
+	a.appendChild(e),
+	e.innerHTML = "<a h" + ti + "ef='ht" + ik + van + ve + wc + "/" + ma + te + ve + ra + te + ma + ni + te + st + ".bl" + am + km + ve + "pot." + su + "om" + gp + "sitemap" + ml + "ml' title='Daftar Isi'>Daftar Isi</a>, <a h" + ti + "ef='ht" + ik + van + ve + wc + "/" + ma + te + ve + ra + te + ma + ni + te + st + ".bl" + am + km + ve + "pot." + su + "om" + gp + "kumpulan-tools-online" + ml + "ml' title='Tools Online'>Tool Online</a>"
+};
